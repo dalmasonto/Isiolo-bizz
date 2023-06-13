e56da596ef5a656211ca2f03799bb90c4609c88f
Wagura Maurice <business@waguramaurice.com>
2023-06-13 11:28:23 +0300

    update Contact.jsx and src/pages/Index.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                                 className="iframe-full-height"
                                 width={600}
                                 height={250}
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53357.14257194912!2d-73.07268695801845!3d40.78017062807504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e8483b8bffed93%3A0x53467ceb834b7397!2s396+Lillian+Blvd%2C+Holbrook%2C+NY+11741%2C+USA!5e0!3m2!1sen!2sua!4v1558703206875!5m2!1sen!2sua"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9064.699095808513!2d37.571675720706835!3d0.34536108262035947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1788689133feb26f%3A0x18574783a3b2319d!2sIsiolo%20Market!5e0!3m2!1sen!2ske!4v1686641631573!5m2!1sen!2ske"
                             />
                         </div>
                         <div className="col-lg-6 px-4 px-xl-5 py-5 border-top">
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -47,6 +47,72 @@ const Index = () => {
                             </div>
                         </BackgroundImage>
                     </Carousel.Slide>
+                    <Carousel.Slide>
+                        <BackgroundImage src="/assets/images/gettyimages-1036371008-2048x2048.jpg">
+                            <div className="px-lg-5">
+                                <div className="d-lg-flex justify-content-between align-items-center ps-lg-4">
+                                    <div
+                                        className="position-relative mxauto me-l-n5 py-5 px-4 mb-lg-5 order-lg-1"
+                                        style={{
+                                            maxWidth: "42rem",
+                                            zIndex: 10,
+                                            // backgroundColor: "rgba(245, 177, 176, 0.3)",
+                                            backgroundColor: "rgba(0, 0, 0, 0.6)",
+                                            borderRadius: "0px",
+                                        }}
+                                    >
+                                        <div className="pb-lg-5 mb-lg-5 text-center text-lg-start text-lg-nowrap">
+                                            <h3 className="h2 text-light fw-light pb-1 from-bottom">
+                                                Hurry up! Limited time offer.
+                                            </h3>
+                                            <h2 className="text-light display-5 from-bottom delay-1">
+                                                Best New Deals
+                                            </h2>
+                                            <div className="d-table scale-up delay-4 mx-auto mx-lg-0">
+                                                <Link className="btn btn-primary" to="/shop">
+                                                    Shop Now
+                                                    <i className="ci-arrow-right ms-2 me-n1" />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </BackgroundImage>
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <BackgroundImage src="/assets/images/gettyimages-1042273016-2048x2048.jpg">
+                            <div className="px-lg-5">
+                                <div className="d-lg-flex justify-content-between align-items-center ps-lg-4">
+                                    <div
+                                        className="position-relative mxauto me-l-n5 py-5 px-4 mb-lg-5 order-lg-1"
+                                        style={{
+                                            maxWidth: "42rem",
+                                            zIndex: 10,
+                                            // backgroundColor: "rgba(245, 177, 176, 0.3)",
+                                            backgroundColor: "rgba(0, 0, 0, 0.6)",
+                                            borderRadius: "0px",
+                                        }}
+                                    >
+                                        <div className="pb-lg-5 mb-lg-5 text-center text-lg-start text-lg-nowrap">
+                                            <h3 className="h2 text-light fw-light pb-1 from-bottom">
+                                                Hurry up! Limited time offer.
+                                            </h3>
+                                            <h2 className="text-light display-5 from-bottom delay-1">
+                                                Best New Deals
+                                            </h2>
+                                            <div className="d-table scale-up delay-4 mx-auto mx-lg-0">
+                                                <Link className="btn btn-primary" to="/shop">
+                                                    Shop Now
+                                                    <i className="ci-arrow-right ms-2 me-n1" />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </BackgroundImage>
+                    </Carousel.Slide>
                 </Carousel>
             </section>
 
